42bc7cbdead16b777850f3777309a62e364cd32e
PhiVanDat123 <phivandat2005@gmail.com>
2025-07-12 20:56:22 +0700

    Create test-sft.sh

---
+++ script/test-sft.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+cd code
+echo "Current directory: $(pwd)"
+
+/usr/local/envs/DSKD-DPO_train/bin/python -u train.py \
+  model=sft \
+  model.policy_name_or_path=Qwen/Qwen2.5-0.5B \
+  model.reference_name_or_path=Qwen/Qwen2.5-0.5B \
+  model.teacher_tokenizer_name_or_path=Qwen/Qwen2.5-0.5B \
+  model.student_tokenizer_name_or_path=Qwen/Qwen2.5-0.5B \
+  model.policy_block_name=Qwen2DecoderLayer \
+  model.reference_block_name=Qwen2DecoderLayer \
+  loss=sft \
+  eval_every=5 \
+  policy_mode=student \
+  datasets=tonyshelby/sample \
+  gradient_accumulation_steps=2 batch_size=4 eval_batch_size=4 \
+  trainer=FSDPTrainer sample_during_eval=false \
+  debug=false \
+  save_repo=tonyshelby/Qwen2.5_0.5B_SFT_sample_log
