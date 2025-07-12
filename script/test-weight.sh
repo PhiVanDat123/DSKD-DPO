51700c0473073247db3a5ede10ac5dac7f5b413d
PhiVanDat123 <phivandat2005@gmail.com>
2025-07-12 21:37:02 +0700

    Create test-weight.sh

---
+++ script/test-weight.sh
@@ -0,0 +1,32 @@
+#!/bin/zsh
+set -e
+
+
+cd code/weight
+echo "Current directory: $(pwd)"
+
+model_name_1="tonyshelby/Qwen2.5_0.5B_SFT_sample"
+model_name_2="openai-community/gpt2"
+# input_dir="datasets/ultra-feedback"
+data_path="pvdhihihi/tis-dpo-5k"
+output_dir="generated-data/ultra-feedback-tisdpo"
+# model1_template="normal"
+# model2_template="normal"
+batch_size=4
+num_gpus=2
+force_sequential=false  # Set to true if multiprocessing causes issues
+split="train"
+
+# Create output directory if it doesn't exist
+# mkdir -p $output_dir
+
+# Run the parallel processing script
+/usr/local/envs/DSKD-DPO_train/bin/python weight.py \
+  --positive_model_name $model_name_1 \
+  --negative_model_name $model_name_2 \
+  --spilt=$split \
+  --input_dir $input_dir \
+  --output_dir $output_dir \
+  --batch_size $batch_size \
+  --num_gpus $num_gpus \
+  $(if $force_sequential; then echo "--force_sequential"; fi) 
