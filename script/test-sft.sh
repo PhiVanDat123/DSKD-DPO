2088ec5e8accfc605862f22c24cf15f7cbccf377
PhiVanDat123 <phivandat2005@gmail.com>
2025-07-18 00:22:38 +0700

    update

--- script/test-sft.sh
+++ script/test-sft.sh
@@ -1,26 +1,28 @@
-  #!/bin/bash
-  set -e
+#!/bin/bash
+set -e
 
-  #cd ..
-  #cd ../script
-  echo "Current directory: $(pwd)"
+echo "Current directory: $(pwd)"
 
-  CONFIG_DIR="/home/hungpv/projects/DSKD-DPO/config"
+CONFIG_DIR="/home/hungpv/projects/DSKD-DPO/config"
 
-  ${CONDA_PREFIX}/bin/python -u ../code/train.py \
-    --config-dir $CONFIG_DIR \
-    --config-name config.yaml \
-    model.policy_name_or_path=Qwen/Qwen2.5-0.5B \
-    model.reference_name_or_path=Qwen/Qwen2.5-0.5B \
-    model.teacher_tokenizer_name_or_path=Qwen/Qwen2.5-0.5B \
-    model.student_tokenizer_name_or_path=Qwen/Qwen2.5-0.5B \
-    model.policy_block_name=Qwen2DecoderLayer \
-    model.reference_block_name=Qwen2DecoderLayer \
-    model.policy_dtype=float32 \
-    loss=sft \
-    eval_every=5 \
-    policy_mode=student \
-    datasets=pvdhihihi/tis-dpo-5k \
-    gradient_accumulation_steps=2 batch_size=4 eval_batch_size=4 \
-    trainer=FSDPTrainer sample_during_eval=false \
-    debug=false \
+${CONDA_PREFIX}/bin/python -u ../code/train.py \
+  --config-dir $CONFIG_DIR \
+  --config-name config.yaml \
+  +model=sft \
+  +model.policy_name_or_path=Qwen/Qwen2.5-0.5B \
+  +model.reference_name_or_path=Qwen/Qwen2.5-0.5B \
+  +model.teacher_tokenizer_name_or_path=Qwen/Qwen2.5-0.5B \
+  +model.student_tokenizer_name_or_path=Qwen/Qwen2.5-0.5B \
+  +model.policy_block_name=Qwen2DecoderLayer \
+  +model.reference_block_name=Qwen2DecoderLayer \
+  +loss=sft \
+  +eval_every=5 \
+  +policy_mode=student \
+  +datasets=pvdhihihi/tis-dpo-5k \
+  +gradient_accumulation_steps=2 \
+  +batch_size=4 \
+  +eval_batch_size=4 \
+  +trainer=FSDPTrainer \
+  +sample_during_eval=false \
+  +debug=false
+
