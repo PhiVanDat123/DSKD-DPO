a2d31664052c588de5e034330a828cd3464b6522
PhiVanDat123 <phivandat2005@gmail.com>
2025-07-18 09:26:58 +0700

    update

--- code/trainers.py
+++ code/trainers.py
@@ -320,15 +320,17 @@ def concatenated_inputs(batch: Dict[str, Union[List, torch.LongTensor]]) -> Dict
     return concatenated_batch
 
 
-class BasicTrainer(nn.Module):
-    def __init__(self, policy: nn.Module, config: DictConfig, seed: int, run_dir: str, reference_model: Optional[nn.Module] = None, rank: int = 0, world_size: int = 1, device: Optional[torch.device] = None):
-        print(f"[crossentropy in trainers] config type: {type(config)}")
-        """A trainer for a language model, supporting either SFT or DPO training.
-            
-            If multiple GPUs are present, naively splits the model across them, effectively
-            offering N times available memory, but without any parallel computation.
-        """
-        super(BasicTrainer, self).__init__()
+class BasicTrainer(object):
+    def __init__(
+        self,
+        policy: nn.Module,
+        seed: int,
+        run_dir: str,
+        config: DictConfig,
+        reference_model: Optional[nn.Module] = None,
+        rank: int = 0,
+        world_size: int = 1,
+    ):
         self.seed = seed
         self.rank = rank
         self.world_size = world_size
@@ -899,9 +901,9 @@ class FSDPTrainer(BasicTrainer):
     def __init__(
         self,
         policy: nn.Module,
-        config: DictConfig,
         seed: int,
         run_dir: str,
+        config: DictConfig,
         reference_model: Optional[nn.Module] = None,
         rank: int = 0,
         world_size: int = 1,
