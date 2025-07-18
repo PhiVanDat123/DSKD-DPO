2e4458b55ee4f579652e19dff035fce19460239b
PhiVanDat123 <phivandat2005@gmail.com>
2025-07-18 20:17:05 +0700

    update

--- code/utils/utils.py
+++ code/utils/utils.py
@@ -114,6 +114,7 @@ def slice_and_move_batch_for_device(batch: Dict, rank: int, world_size: int, dev
         raise TypeError(f"Unsupported batch type: {type(batch)}")
     start = chunk_size * rank
     end = chunk_size * (rank + 1)
+    print(f"[slice_and_move_batch_for_device] rank {rank} batch.items()")
     sliced = {k: v[start:end] for k, v in batch.items()}
     on_device = {k: (v.to(device) if isinstance(v, torch.Tensor) else v) for k, v in sliced.items()}
     return on_device
