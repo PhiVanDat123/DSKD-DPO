529fd0dea64492f31bbcc89b412d0dedf1808067
PhiVanDat123 <phivandat2005@gmail.com>
2025-07-19 16:54:07 +0700

    update

--- code/criterions/dual_space_kd_with_cross_model_attention.py
+++ code/criterions/dual_space_kd_with_cross_model_attention.py
@@ -5,30 +5,92 @@ from typing import Dict, List, Union
 from utils.utils import pad_to_length
 from datasets import load_dataset
 
-def concatenated_inputs(batch: Dict[str, Union[List, torch.LongTensor]]) -> Dict[str, torch.LongTensor]:
+def fast_pad_tensor(input_tensor, max_token, max_span, pad_value=-1):
+    batch_size, token_size, span_size = input_tensor.shape
+
+    # Create the output tensor filled with pad_value
+    output = input_tensor.new_full((batch_size, max_token, max_span), pad_value)
+
+    # Copy the original values into the top-left part
+    output[:, :token_size, :span_size] = input_tensor
+
+    return output
+
+def concatenated_inputs(batch: Dict, mode: str) -> Dict[str, torch.LongTensor]:
     """Concatenate the chosen and rejected inputs into a single tensor.
-    
+
     Args:
         batch: A batch of data. Must contain the keys 'chosen_input_ids' and 'rejected_input_ids', which are tensors of shape (batch_size, sequence_length).
-        
+
     Returns:
         A dictionary containing the concatenated inputs under the key 'concatenated_input_ids'.
     """
-    max_length = max(batch['chosen_input_ids'].shape[1], batch['rejected_input_ids'].shape[1])
+    max_length = max(
+        batch[f"chosen_{mode}_input_ids"].shape[1], batch[f"rejected_{mode}_input_ids"].shape[1]
+    )
+    max_num_parents = max(
+        batch[f"chosen_{mode}_parent_list"].shape[1], batch[f"rejected_{mode}_parent_list"].shape[1]
+    )
+    max_span = max(
+        batch[f"chosen_{mode}_parent_list"].shape[2], batch[f"rejected_{mode}_parent_list"].shape[2]
+    )
     concatenated_batch = {}
-    for k in batch:
-        if k.startswith('chosen') and isinstance(batch[k], torch.Tensor):
-            pad_value = -100 if 'labels' in k else 0
-            concatenated_key = k.replace('chosen', 'concatenated')
-            concatenated_batch[concatenated_key] = pad_to_length(batch[k], max_length, pad_value=pad_value)
-    for k in batch:
-        if k.startswith('rejected') and isinstance(batch[k], torch.Tensor):
-            pad_value = -100 if 'labels' in k else 0
-            concatenated_key = k.replace('rejected', 'concatenated')
-            concatenated_batch[concatenated_key] = torch.cat((
-                concatenated_batch[concatenated_key],
-                pad_to_length(batch[k], max_length, pad_value=pad_value),
-            ), dim=0)
+    keys = [k for k in batch if mode in k]
+    keys.extend([k for k in batch if "weight" in k])
+    for k in keys:
+        # if k.startswith("chosen") and isinstance(batch[k], torch.Tensor):
+        if k.startswith("chosen"):
+            pad_value = -100 if "labels" in k else 0
+            concatenated_key = k.replace("chosen", "concatenated")
+            if "weight" in k:
+                # print(k)
+                # print(concatenated_key)
+                concatenated_batch[concatenated_key] = pad_to_length(
+                    batch[k], max_num_parents, pad_value=pad_value
+                )
+            elif "parent_list" in k:
+                concatenated_batch[concatenated_key] = fast_pad_tensor(
+                    batch[k], max_num_parents, max_span, pad_value=-1
+                )
+            elif ("parent_dict" in k) or ("offset_mapping" in k):
+                concatenated_batch[concatenated_key] = batch[k]
+            else:
+                # print(k)
+                # print(type(batch[k]))
+                concatenated_batch[concatenated_key] = pad_to_length(
+                    batch[k], max_length, pad_value=pad_value
+                )
+    for k in keys:
+        # if k.startswith("rejected") and isinstance(batch[k], torch.Tensor):
+        if k.startswith("rejected"):
+            pad_value = -100 if "labels" in k else 0
+            concatenated_key = k.replace("rejected", "concatenated")
+            if "weight" in k:
+                concatenated_batch[concatenated_key] = torch.cat(
+                    (
+                        concatenated_batch[concatenated_key],
+                        pad_to_length(batch[k], max_num_parents, pad_value=pad_value),
+                    ),
+                    dim=0,
+                )
+            elif "parent_list" in k:
+                concatenated_batch[concatenated_key] = torch.cat(
+                    (
+                        concatenated_batch[concatenated_key],
+                        fast_pad_tensor(batch[k], max_num_parents, max_span, pad_value=-1),
+                    ),
+                    dim=0,
+                )
+            elif ("parent_dict" in k) or ("offset_mapping" in k):
+                concatenated_batch[concatenated_key] += batch[k]
+            else:
+                concatenated_batch[concatenated_key] = torch.cat(
+                    (
+                        concatenated_batch[concatenated_key],
+                        pad_to_length(batch[k], max_length, pad_value=pad_value),
+                    ),
+                    dim=0,
+                )
     return concatenated_batch
 
 class DualSpaceKDWithCMA(VariousDivergence):
