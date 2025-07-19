618c6a2cc796ae1619635e8a15e28927ffeb95b6
PhiVanDat123 <phivandat2005@gmail.com>
2025-07-19 22:07:37 +0700

    Create student-sft.sh

---
+++ script/student-sft.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+#cd ..
+#cd ../script
+echo "Current directory: $(pwd)"
+
+CONFIG_DIR="/home/hungpv/projects/DSKD-DPO/config"
+
+${CONDA_PREFIX}/bin/python -u ../code/train.py \
+  --config-dir $CONFIG_DIR \
+  --config-name config.yaml \
+  model.policy_name_or_path=meta-llama/Llama-3.1-8B \
+  model.reference_name_or_path=Qwen/Qwen2.5-14B \
+  model.teacher_tokenizer_name_or_path=Qwen/Qwen2.5-14B \
+  model.student_tokenizer_name_or_path=meta-llama/Llama-3.1-8B \
+  model.policy_block_name=Qwen2DecoderLayer \
+  model.reference_block_name=MistralDecoderLayer \
+  loss=sft \
+  eval_every=5 \
+  policy_mode=student \
+  datasets=tonyshelby/ultra-feedback_checking \
+  gradient_accumulation_steps=2 batch_size=4 eval_batch_size=4 \
+  trainer=FSDPTrainer sample_during_eval=false \
+  debug=false \
