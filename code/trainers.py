dd8b364e6566523aa80ef88a313f52af73a15aef
PhiVanDat123 <phivandat2005@gmail.com>
2025-07-19 16:05:43 +0700

    update

--- code/trainers.py
+++ code/trainers.py
@@ -858,6 +858,7 @@ class BasicTrainer(object):
 
                 for eval_batch in (tqdm.tqdm(self.eval_batches, desc='Computing eval metrics') if self.rank == 0 else self.eval_batches):
                     local_eval_batch = slice_and_move_batch_for_device(eval_batch, self.rank, self.world_size, self.rank)
+                    print("[eval] Local eval batch content:", local_eval_batch)
                     with torch.no_grad():
                         _, eval_metrics = self.get_batch_metrics(local_eval_batch, self.config.loss, train=False)
 
