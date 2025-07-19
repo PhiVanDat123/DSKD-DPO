2d4e7b17764c0e6a0c82792ca07a84d341f97516
PhiVanDat123 <phivandat2005@gmail.com>
2025-07-20 00:46:41 +0700

    update

--- script_test/test-weight.sh
+++ script_test/test-weight.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-
-#cd code/weight
 echo "Current directory: $(pwd)"
 
 CONFIG_DIR="/home/hungpv/projects/DSKD-DPO/config"
@@ -12,17 +10,11 @@ model_name_2="openai-community/gpt2"
 input_dir="datasets/ultra-feedback"
 data_path="tonyshelby/ultra-feedback_checking"
 output_dir="generated-data/ultra-feedback-tisdpo"
-# model1_template="normal"
-# model2_template="normal"
 batch_size=4
 num_gpus=2
 force_sequential=false  # Set to true if multiprocessing causes issues
 split="train"
 
-# Create output directory if it doesn't exist
-# mkdir -p $output_dir
-
-# Run the parallel processing script
 ${CONDA_PREFIX}/bin/python -u ../code/weight.py \
   --positive_model_name $model_name_1 \
   --negative_model_name $model_name_2 \
@@ -31,4 +23,5 @@ ${CONDA_PREFIX}/bin/python -u ../code/weight.py \
   --output_dir $output_dir \
   --batch_size $batch_size \
   --num_gpus $num_gpus \
-  $(if $force_sequential; then echo "--force_sequential"; fi) 
+  $( [ "$force_sequential" = true ] && echo "--force_sequential" )
+
