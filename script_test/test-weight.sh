a116e808e025ede731dbdcc6d2e58b00cd98a03e
PhiVanDat123 <phivandat2005@gmail.com>
2025-07-20 14:33:50 +0700

    update

--- script_test/test-weight.sh
+++ script_test/test-weight.sh
@@ -7,7 +7,7 @@ CONFIG_DIR="/home/hungpv/projects/DSKD-DPO/config"
 
 model_name_1="tonyshelby/Qwen2.5_0.5B_SFT_sample"
 model_name_2="openai-community/gpt2"
-input_dir="datasets/ultra-feedback"
+#input_dir="datasets/ultra-feedback"
 data_path="tonyshelby/ultra-feedback_checking"
 output_dir="generated-data/ultra-feedback-tisdpo"
 batch_size=4
@@ -21,7 +21,6 @@ ${CONDA_PREFIX}/bin/python -u ../code/weight.py \
   --negative_model_name $model_name_2 \
   --data_path $data_path \
   --split=$split \
-  --input_dir $input_dir \
   --output_dir $output_dir \
   --batch_size $batch_size \
   --num_gpus $num_gpus \
