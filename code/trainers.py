0d49c593385e01b44e6039f22adce77ce94596cb
PhiVanDat123 <phivandat2005@gmail.com>
2025-07-21 23:44:02 +0700

    update

--- code/trainers.py
+++ code/trainers.py
@@ -984,7 +984,7 @@ class BasicTrainer(object):
                     #print(f"[DEBUG] local_microbatch keys: {list(local_microbatch.keys())}")
                     concat_student_data = concatenated_inputs(local_microbatch, mode='student')
                     concat_teacher_data = concatenated_inputs(local_microbatch, mode='teacher')
-                    t2s_logits, target = self.DSKD.compute_dual_space_kd_loss_with_cma(self, concat_student_data, concat_teacher_data, distiller, self.policy, self.reference_model)
+                    t2s_logits, target = self.DSKD.compute_dual_space_kd_loss_with_cma(concat_student_data, concat_teacher_data, distiller, self.policy, self.reference_model)
 
                     #  Projector loss vẫn cần tính gradient
                     projector_loss, _ = self.loss.compute_cross_entropy_loss(t2s_logits, target)
