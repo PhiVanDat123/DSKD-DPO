54c11fa45ca8bf63aa330f3fc3d5f24855b8c233
PhiVanDat123 <phivandat2005@gmail.com>
2025-07-25 17:22:36 +0700

    update

--- script/DSKD-DPO.sh
+++ script/DSKD-DPO.sh
@@ -22,7 +22,7 @@ ${CONDA_PREFIX}/bin/python -u ../code/train.py \
   reference_mode=teacher \
   eval_every=500 \
   datasets=pvdhihihi/14B-weight-trasformed-v2 \
-  gradient_accumulation_steps=1 batch_size=16 eval_batch_size=16 \
+  gradient_accumulation_steps=1 batch_size=8 eval_batch_size=8 \
   trainer=FSDPTrainer sample_during_eval=false \
 #  save_repo=tonyshelby/Qwen2.5_0.5B_TDPO_DSKD \
 #   model.policy_name_or_path=openai-community/gpt2 \
