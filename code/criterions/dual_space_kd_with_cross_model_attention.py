05f2102f71569c8baefcdb1cf8f852cb7f60ae79
PhiVanDat123 <phivandat2005@gmail.com>
2025-08-08 20:31:26 +0700

    update

--- code/criterions/dual_space_kd_with_cross_model_attention.py
+++ code/criterions/dual_space_kd_with_cross_model_attention.py
@@ -191,6 +191,7 @@ class DualSpaceKDWithCMA(VariousDivergence):
         
         return t2s_logits, target
     
+    '''
     def compute_dual_space_kd_loss_with_cma(
         self, batch, distiller, model, reference_model
     ):  
@@ -212,8 +213,8 @@ class DualSpaceKDWithCMA(VariousDivergence):
             attention_mask=batch[f"chosen_teacher_attention_mask"].to(device),
             #position_ids=concat_input_data.get(f"teacher_{distiller.teacher_model_type}_position_ids", None), 
             output_hidden_states=True)
-            
-        '''
+    '''      
+    '''
         with torch.no_grad():
             model.eval()
             outputs = model(
@@ -221,7 +222,8 @@ class DualSpaceKDWithCMA(VariousDivergence):
                 attention_mask=concat_student_data[f"concatenated_student_attention_mask"].to(device),
                 output_hidden_states=True).logits.to(device)
             print("[DEBUG] outputs logits shape:", outputs.shape)
-        '''
+    '''
+    '''
         #concat_output_data = concatenated_inputs(output_data)
         target = batch["chosen_student_labels"].to(device)
         teacher_target = batch["chosen_teacher_labels"].to(device)
@@ -248,7 +250,149 @@ class DualSpaceKDWithCMA(VariousDivergence):
         print("[dskd] stu_embed_tokens device (after .to):", stu_embed_tokens.weight.device)
         print(f"[dskd] stu_embed_tokens.is_meta: {stu_embed_tokens.weight.is_meta}")
         print("[dskd] weight.storage size:", stu_embed_tokens.weight.storage().size())
+    '''
+        
 
+    '''        
+        if hasattr(teacher_model, "model") \
+            and hasattr(teacher_model.model, "embed_tokens"):
+            tea_embed_tokens = teacher_model.model.embed_tokens
+        elif hasattr(teacher_model, "model") \
+            and hasattr(teacher_model.model, "model") \
+            and hasattr(teacher_model.model.model, "embed_tokens"):
+            tea_embed_tokens = teacher_model.model.model.embed_tokens
+        elif hasattr(teacher_model, "transformer") \
+            and hasattr(teacher_model.model, "wte"):
+            tea_embed_tokens = teacher_model.transformer.wte
+        else:
+            raise NotImplementedError
+    '''
+    '''    
+        tea_embed_tokens = getattr(
+            getattr(teacher_model, "module", teacher_model).transformer,
+            "word_embeddings"
+        )
+            
+        tea_embed_tokens = tea_embed_tokens.to(device)
+        print("[dskd] tea_embed_tokens device:", tea_embed_tokens.weight.device)
+            
+
+        formal_target = torch.where(pad_mask, target, torch.zeros_like(target)).to(device)
+        print("[dskd] formal_target device:", formal_target.device)
+        formal_input = torch.where(pad_mask, batch["chosen_student_input_ids"].to(device), torch.zeros_like(target)).to(device)
+        print("[dskd] formal_input device:", formal_input.device)
+        print("formal_input:", formal_input)
+        stu_input_embeds = stu_embed_tokens(formal_input).detach()   
+        stu_target_embeds = stu_embed_tokens(formal_target).detach().to(device)
+
+        formal_teacher_target = torch.where(teacher_pad_mask, teacher_target, torch.zeros_like(teacher_target)).to(device)
+        formal_teacher_input = torch.where(teacher_pad_mask, batch[f"chosen_teacher_input_ids"].to(device), torch.zeros_like(teacher_target)).to(device)
+        tea_input_embeds = tea_embed_tokens(formal_teacher_input).detach().to(device)
+        tea_target_embeds = tea_embed_tokens(formal_teacher_target).detach().to(device)
+
+        stu_index_embeds = torch.cat([stu_input_embeds, stu_target_embeds], -1).to(device)
+        tea_index_embeds = torch.cat([tea_input_embeds, tea_target_embeds], -1).to(device)
+
+        norm_tea_index_embeds = tea_index_embeds / tea_index_embeds.std()
+        norm_tea_target_embeds = tea_target_embeds / tea_target_embeds.std()
+        norm_teacher_hiddens = teacher_hiddens / teacher_hiddens.std()
+
+        print("stu_index_embeds.shape:", stu_index_embeds.shape)
+        print("tea_index_embeds.shape:", tea_index_embeds.shape)
+        print("projectors['query']:", distiller.projectors["query"])
+
+        distiller.projectors["query"] = distiller.projectors["query"].to(device)
+        stu_q_hiddens = distiller.projectors["query"](stu_index_embeds).float().to(device)
+        tea_k_hiddens = norm_tea_index_embeds.float().to(device)
+
+        print("projectors['t2s']:", distiller.projectors["t2s"])
+        print("(norm_teacher_hiddens + norm_tea_target_embeds).shape:", (norm_teacher_hiddens + norm_tea_target_embeds).shape)
+
+        distiller.projectors["t2s"] = distiller.projectors["t2s"].to(device)
+        tea_v_hiddens = distiller.projectors["t2s"](
+            norm_teacher_hiddens + norm_tea_target_embeds
+        ).float().to(device)
+        
+        print("stu_q_hiddens.shape:", stu_q_hiddens.shape)
+        print("tea_k_hiddens.shape:", tea_k_hiddens.shape)
+        align = stu_q_hiddens.matmul(tea_k_hiddens.transpose(-1, -2))
+        align = align / math.sqrt(2 * teacher_hiddens.shape[-1])
+        align_mask = pad_mask.float().unsqueeze(-1) * teacher_pad_mask.float().unsqueeze(1)
+        align = align + (1.0 - align_mask) * (-100000)
+        print("[DEBUG] align shape:", align.shape)
+
+        t2s_weight = torch.softmax(align, -1) 
+        print("[DEBUG] t2s_weight shape:", t2s_weight.shape)
+        print("[DEBUG] tea_v_hiddens shape:", tea_v_hiddens.shape)       
+        t2s_hiddens = t2s_weight.matmul(tea_v_hiddens)
+        print("[DEBUG] t2s_hiddens shape:", t2s_hiddens.shape)
+        print("[DEBUG] model.lm_head.weight.detach().transpose(-1, -2) shape", model.lm_head.weight.detach().transpose(-1, -2).shape)
+        t2s_logits = t2s_hiddens.matmul(
+            model.lm_head.weight.detach().transpose(-1, -2).to(device)
+        ).to(device)
+        print("[DEBUG] t2s_logits shape:", t2s_logits.shape)
+        
+        return t2s_logits, target
+    '''
+    def compute_dual_space_kd_loss_with_cma(
+        self, batch, distiller, model, reference_model
+    ):  
+        device = next(model.parameters()).device  # Lấy device của mô hình
+        #batch = {k: v.to(device) for k, v in batch.items() if torch.is_tensor(v)}  # Di chuyển các tensor trong batch
+        self.distiller = distiller
+        model = model.to(device)
+        print("[dskd] Model device:", model.device)
+        teacher_model = reference_model.to(device)
+        print("[dskd] Teacher model device:", teacher_model.device)
+        #print("[DEBUG] Max input ID:", concat_student_data["concatenated_student_input_ids"].max().item())
+        #print("[DEBUG] Vocab size:", model.config.vocab_size)
+        #print("[DEBUG] Input IDs shape:", concat_student_data["concatenated_student_input_ids"].shape)
+
+        
+        teacher_model.eval()
+        teacher_outputs = teacher_model(
+            batch["chosen_teacher_input_ids"].to(device),
+            attention_mask=batch[f"chosen_teacher_attention_mask"].to(device),
+            #position_ids=concat_input_data.get(f"teacher_{distiller.teacher_model_type}_position_ids", None), 
+            output_hidden_states=True)
+        
+        '''
+        with torch.no_grad():
+            model.eval()
+            outputs = model(
+                concat_student_data["concatenated_student_input_ids"].to(device),
+                attention_mask=concat_student_data[f"concatenated_student_attention_mask"].to(device),
+                output_hidden_states=True).logits.to(device)
+            print("[DEBUG] outputs logits shape:", outputs.shape)
+        '''
+    
+        #concat_output_data = concatenated_inputs(output_data)
+        target = batch["chosen_student_labels"].to(device)
+        teacher_target = batch["chosen_teacher_labels"].to(device)
+            
+        pad_mask = target.ne(self.padding_id).to(device)
+        teacher_pad_mask = teacher_target.ne(self.padding_id).to(device)
+
+        teacher_hiddens = teacher_outputs.hidden_states[-1].to(device)
+
+        if hasattr(model, "model") \
+            and hasattr(model.model, "embed_tokens"):
+            stu_embed_tokens = model.model.embed_tokens
+        elif hasattr(model, "model") \
+            and hasattr(model.model, "model") \
+            and hasattr(model.model.model, "embed_tokens"):
+            stu_embed_tokens = model.model.model.embed_tokens
+        elif hasattr(model, "transformer") \
+            and hasattr(model.transformer, "word_embeddings"):
+            stu_embed_tokens = model.transformer.word_embeddings
+        else:
+            raise NotImplementedError
+
+        #stu_embed_tokens = stu_embed_tokens.to(device)
+        print("[dskd] stu_embed_tokens device (after .to):", stu_embed_tokens.weight.device)
+        print(f"[dskd] stu_embed_tokens.is_meta: {stu_embed_tokens.weight.is_meta}")
+        print("[dskd] weight.storage size:", stu_embed_tokens.weight.storage().size())
+    
         
 
         '''        
@@ -271,22 +415,45 @@ class DualSpaceKDWithCMA(VariousDivergence):
             "word_embeddings"
         )
             
-        tea_embed_tokens = tea_embed_tokens.to(device)
+        #tea_embed_tokens = tea_embed_tokens.to(device)
         print("[dskd] tea_embed_tokens device:", tea_embed_tokens.weight.device)
-            
+
+        try:
+            distiller.projectors.to(device)
+        except Exception:
+            # in case projectors is a dict-like of nn.Modules
+            for k, m in distiller.projectors.items():
+                try:
+                    m.to(device)
+                except Exception:
+                    pass    
+        
+        stu_embed_tokens = stu_embed_tokens.to(device)
+        tea_embed_tokens = tea_embed_tokens.to(device)
 
         formal_target = torch.where(pad_mask, target, torch.zeros_like(target)).to(device)
         print("[dskd] formal_target device:", formal_target.device)
         formal_input = torch.where(pad_mask, batch["chosen_student_input_ids"].to(device), torch.zeros_like(target)).to(device)
         print("[dskd] formal_input device:", formal_input.device)
         print("formal_input:", formal_input)
-        stu_input_embeds = stu_embed_tokens(formal_input).detach()   
-        stu_target_embeds = stu_embed_tokens(formal_target).detach().to(device)
+        #stu_input_embeds = stu_embed_tokens(formal_input).detach()   
+        #stu_target_embeds = stu_embed_tokens(formal_target).detach().to(device)
 
         formal_teacher_target = torch.where(teacher_pad_mask, teacher_target, torch.zeros_like(teacher_target)).to(device)
         formal_teacher_input = torch.where(teacher_pad_mask, batch[f"chosen_teacher_input_ids"].to(device), torch.zeros_like(teacher_target)).to(device)
-        tea_input_embeds = tea_embed_tokens(formal_teacher_input).detach().to(device)
-        tea_target_embeds = tea_embed_tokens(formal_teacher_target).detach().to(device)
+        #tea_input_embeds = tea_embed_tokens(formal_teacher_input).detach().to(device)
+        #tea_target_embeds = tea_embed_tokens(formal_teacher_target).detach().to(device)
+
+        with torch.no_grad():
+            stu_input_embeds = stu_embed_tokens(formal_input).clone()
+            stu_target_embeds = stu_embed_tokens(formal_target).clone()
+            tea_input_embeds = tea_embed_tokens(formal_teacher_input).clone()
+            tea_target_embeds = tea_embed_tokens(formal_teacher_target).clone()
+        
+        stu_input_embeds = stu_input_embeds.to(device)
+        stu_target_embeds = stu_target_embeds.to(device)
+        tea_input_embeds = tea_input_embeds.to(device)
+        tea_target_embeds = tea_target_embeds.to(device)
 
         stu_index_embeds = torch.cat([stu_input_embeds, stu_target_embeds], -1).to(device)
         tea_index_embeds = torch.cat([tea_input_embeds, tea_target_embeds], -1).to(device)
