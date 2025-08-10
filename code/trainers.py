a63ebf822a3373b0427dd520306406ef7fff0f88
PhiVanDat123 <phivandat2005@gmail.com>
2025-08-10 20:27:20 +0700

    update

--- code/trainers.py
+++ code/trainers.py
@@ -453,8 +453,8 @@ class BasicTrainer(object):
         self.loss = CrossEntropyLoss(config, padding_id=-100)
         self.DSKD = DualSpaceKDWithCMA(config, padding_id=-100)
 
-        device = next(policy.parameters()).device
-        self.distiller = Distiller(config, device)
+        self.device = next(policy.parameters()).device
+        self.distiller = Distiller(config, self.device)
 
         teacher_tokenizer_name_or_path = (
             config.model.teacher_tokenizer_name_or_path or config.model.teacher_name_or_path
@@ -890,7 +890,7 @@ class BasicTrainer(object):
     '''
 
     def train(self, config):
-        distiller = Distiller(config)
+        distiller = Distiller(config, self.device)
         rank0_print(f'Using {self.config.optimizer} optimizer')
         self.optimizer = getattr(torch.optim, self.config.optimizer)(
             self.policy.parameters(), lr=self.config.lr
