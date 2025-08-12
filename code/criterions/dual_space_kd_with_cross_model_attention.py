b33d27acd643e3a794db25860dcb696cd517e9a0
PhiVanDat123 <phivandat2005@gmail.com>
2025-08-12 10:03:09 +0700

    update

--- code/criterions/dual_space_kd_with_cross_model_attention.py
+++ code/criterions/dual_space_kd_with_cross_model_attention.py
@@ -108,7 +108,7 @@ class DualSpaceKDWithCMA(VariousDivergence):
         stu_embed_tokens = stu_embed_tokens.to(device)
 
 
-        '''
+        
         if hasattr(teacher_model, "model") \
             and hasattr(teacher_model.model, "embed_tokens"):
             tea_embed_tokens = teacher_model.model.embed_tokens
@@ -121,13 +121,13 @@ class DualSpaceKDWithCMA(VariousDivergence):
             tea_embed_tokens = teacher_model.transformer.wte
         else:
             raise NotImplementedError
-        '''
         
+        '''
         tea_embed_tokens = getattr(
             getattr(teacher_model, "module", teacher_model).transformer,
             "word_embeddings"
         )
-        
+        '''
         tea_embed_tokens = tea_embed_tokens.to(device)
             
 
@@ -231,7 +231,7 @@ class DualSpaceKDWithCMA(VariousDivergence):
         stu_embed_tokens = stu_embed_tokens.to(device)
     
         
-        '''
+        
         if hasattr(teacher_model, "model") \
             and hasattr(teacher_model.model, "embed_tokens"):
             tea_embed_tokens = teacher_model.model.embed_tokens
@@ -244,13 +244,13 @@ class DualSpaceKDWithCMA(VariousDivergence):
             tea_embed_tokens = teacher_model.transformer.wte
         else:
             raise NotImplementedError
-        '''
         
+        '''
         tea_embed_tokens = getattr(
             getattr(teacher_model, "module", teacher_model).transformer,
             "word_embeddings"
         )
-        
+        '''
         tea_embed_tokens = tea_embed_tokens.to(device)
             
 
