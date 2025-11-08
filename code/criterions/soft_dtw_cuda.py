011d6b22899fa6b6dd58d3556d12b8f7ce862e40
PhiVanDat123 <phivandat2005@gmail.com>
2025-11-08 10:15:03 +0700

    fixxx

--- code/criterions/soft_dtw_cuda.py
+++ code/criterions/soft_dtw_cuda.py
@@ -88,7 +88,7 @@ class _SoftDTWCUDA(Function):
         # max threads per block
         if D.is_cuda:
             props = torch.cuda.get_device_properties(dev)
-            max_threads = props.max_threads_per_block
+            max_threads = props.max_threads
         else:
             max_threads = 1024
         SAFE_THREAD_CAP = min(max_threads, 256)
